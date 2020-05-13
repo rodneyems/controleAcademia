c5ef825da11e7dbaa291584baa62829765df13ac
rodneyems <rodney.martins@live.com>
2020-05-13 11:20:55 -0300

    Updating style, routes, index and layout (Due the form)

--- routes.js
+++ routes.js
@@ -4,11 +4,19 @@ const routes = express.Router()
 /* ====== ROUTES ====== */
 
 routes.get('/', function(req, res){
-    return res.redirect("/instrutores/index.njk")
+    return res.redirect("/instrutores")
 })
 
-routes.get('/instrutores/index.njk', function(req, res){
+routes.get('/instrutores', function(req, res){
     return res.render("instrutores/index")
+})
+
+routes.get('/instrutores/create.njk', function(req, res){
+    return res.render("instrutores/create.njk")
+})
+
+routes.post('/instrutores', function(req, res){
+    return res.send("recebido")
 })
 
 routes.get('/membros', function(req, res){
